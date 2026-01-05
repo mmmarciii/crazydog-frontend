03aad6a711f5e241acc3e4428cf7ee6570fa99cd
mmmarciii <mesz.marc@gmail.com>
2026-01-05 13:13:31 +0100

    Send feedback about the email to customer

--- public/mail.php
+++ public/mail.php
@@ -1,34 +1,59 @@
 <?php
-// CORS
+// CORS beállítások
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// If OPTIONS request (preflight), stop
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
 
-// Angular send the data in JSON format
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
 if (!empty($_POST['email']) && !empty($_POST['message'])) {
     
-    $to = "info@crazydogcustom.com"; 
-    $subject = "You have a new message: " . $_POST['subject'];
+    $user_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $user_name = strip_tags($_POST['name']);
+    $subject_orig = strip_tags($_POST['subject']);
+    $message_orig = strip_tags($_POST['message']);
+
+    // --- 1. EMAIL: for admin ---
+    $to_admin = "info@crazydogcustom.com"; 
+    $subject_admin = "New message from website: " . $subject_orig;
+    $content_admin = "You have a new message via contact form:\n\n";
+    $content_admin .= "Name: " . $user_name . "\n";
+    $content_admin .= "Email: " . $user_email . "\n\n";
+    $content_admin .= "Message:\n" . $message_orig;
+
+    $headers_admin = "From: info@crazydogcustom.com\r\n";
+    $headers_admin .= "Reply-To: " . $user_email . "\r\n";
+    $headers_admin .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $admin_mail_sent = mail($to_admin, $subject_admin, $content_admin, $headers_admin);
+
+    // --- 2. EMAIL: Customer  ---
+    $subject_user = "Confirmation: We received your message - Crazy Dog Custom";
     
-    $email_content = "Name: " . $_POST['name'] . "\n";
-    $email_content .= "Email: " . $_POST['email'] . "\n\n";
-    $email_content .= "Message:\n" . $_POST['message'];
+    $content_user = "Dear " . $user_name . ",\n\n";
+    $content_user .= "Thank you for contacting us! This is an automated confirmation that we have received your message.\n\n";
+    $content_user .= "--- Your Message ---\n";
+    $content_user .= "Subject: " . $subject_orig . "\n";
+    $content_user .= "Message: " . $message_orig . "\n\n";
+    $content_user .= "We will get back to you as soon as possible.\n\n";
+    $content_user .= "Best regards,\n";
+    $content_user .= "The Crazy Dog Custom Team\n";
+    $content_user .= "https://crazydogcustom.com";
 
-    $headers = "From: info@crazydogcustom.com\r\n";
-    $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers_user = "From: info@crazydogcustom.com\r\n";
+    $headers_user .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    if (mail($to, $subject, $email_content, $headers)) {
+    mail($user_email, $subject_user, $content_user, $headers_user);
+
+    // Responses to Angular
+    if ($admin_mail_sent) {
         http_response_code(200);
-        echo json_encode(["status" => "success", "message" => "Email send!"]);
+        echo json_encode(["status" => "success", "message" => "Emails sent!"]);
     } else {
         http_response_code(500);
         echo json_encode(["status" => "error", "message" => "Server failure."]);
