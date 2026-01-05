06f72239484d93d9dfea5fe6ab66b3fa39be47e6
mmmarciii <mesz.marc@gmail.com>
2026-01-05 13:18:29 +0100

    Send feedback about the email to customer

--- public/mail.php
+++ public/mail.php
@@ -1,5 +1,5 @@
 <?php
-// CORS beállítások
+// CORS
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
