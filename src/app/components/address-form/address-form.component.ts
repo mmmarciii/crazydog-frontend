cf68007801516ecb6bc80876e4a72e0019598176
mmmarciii <mesz.marc@gmail.com>
2026-01-17 18:15:00 +0100

    Automaic city fill based on zip code and country code in address form component

--- src/app/components/address-form/address-form.component.ts
+++ src/app/components/address-form/address-form.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, OnInit, inject, DestroyRef } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-address-form',
@@ -8,9 +11,54 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './address-form.component.html',
   styleUrl:'../quote-form/quote-form.component.css'
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements OnInit{
   @Input() parentForm!: FormGroup;
   @Input() prefix: string = '';
+
+  private http = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  private countryCodes: { [key: string]: string } = {
+    'Hungary': 'HU',
+    'Austria': 'AT',
+    'Germany': 'DE'
+  };
+
+  ngOnInit() {
+    this.setupZipLookup();
+  }
+
+  private setupZipLookup() {
+    const zipControl = this.parentForm.get(this.prefix + 'Zip');
+    const countryControl = this.parentForm.get(this.prefix + 'Country');
+
+    if (!zipControl || !countryControl) return;
+
+    zipControl.valueChanges.pipe(
+      debounceTime(400),           
+      distinctUntilChanged(),      
+      filter(zip => zip && zip.length >= 4), 
+      switchMap(zip => {
+        const countryName = countryControl.value;
+        const code = this.countryCodes[countryName] || 'HU';
+
+        return this.http.get(`https://api.zippopotam.us/${code}/${zip}`).pipe(
+          catchError(() => {
+            return of(null); 
+          })
+        );
+      }),
+      takeUntilDestroyed(this.destroyRef) 
+    ).subscribe({
+      next: (data: any) => {
+        if (data && data.places && data.places.length > 0) {
+          this.parentForm.patchValue({
+            [this.prefix + 'City']: data.places[0]['place name']
+          });
+        }
+      }
+    });
+  }
 
   isFieldInvalid(fieldName: string): boolean {
   const control = this.parentForm.get(this.prefix + fieldName);
