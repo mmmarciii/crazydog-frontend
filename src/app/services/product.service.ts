22a13d022d5426f458d9df4ec414d2b0229175e6
mmmarciii <mesz.marc@gmail.com>
2026-01-31 20:39:21 +0100

    Bug fixing

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,7 +8,8 @@ export class ProductService {
   private products = [
     { 
       name: 'OURPRODUCTS.PROD1', 
-      link: 'images/products/basic.jpg', 
+      link: 'images/products/basic.jpg',
+      value: 'Basic', 
       desc: 'OURPRODUCTS.PROD1_DESC',
       colors: 'OURPRODUCTS.PROD1_DET_1',
       areas: 'OURPRODUCTS.PROD1_DET_2',
@@ -16,7 +17,8 @@ export class ProductService {
       desing: 'OURPRODUCTS.PROD1_DET_4'
     },
     { 
-      name: 'OURPRODUCTS.PROD2', 
+      name: 'OURPRODUCTS.PROD2',
+      value: 'Pro', 
       link: 'images/products/labubu-v4-5-4.jpg', 
       desc: 'OURPRODUCTS.PROD2_DESC',
       colors: 'OURPRODUCTS.PROD2_DET_1',
@@ -24,7 +26,8 @@ export class ProductService {
       patterns: 'OURPRODUCTS.PROD2_DET_3',
       desing: 'OURPRODUCTS.PROD2_DET_4'
     }, 
-    { name: 'OURPRODUCTS.PROD3', 
+    { name: 'OURPRODUCTS.PROD3',
+      value: 'Elite', 
       link: 'images/products/star-wars-v6-5-4.jpg', 
       desc: 'OURPRODUCTS.PROD3_DESC',
       colors: 'OURPRODUCTS.PROD3_DET_1',
